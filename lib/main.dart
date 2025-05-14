a841787a586393101c3d4276feab7ebee335093e
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-05-14 16:23:20 +0500

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(create:(_)=> LightBloc(),child: MaterialApp(home: SplashScreen(),),);
+    return BlocProvider(
+      create: (_) => LightBloc(),
+      child: MaterialApp(home: SplashScreen()),
+    );
   }
 }
