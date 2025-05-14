15983fbf27094f662118f82b216150643922a82e
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-05-14 16:23:39 +0500

    update bloc

--- lib/bloc/light_bloc.dart
+++ lib/bloc/light_bloc.dart
@@ -2,10 +2,10 @@ import 'package:app/bloc/light_event.dart';
 import 'package:app/bloc/light_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LightBloc extends Bloc<LightEvent,LightState>{
- LightBloc() : super(LightState(isOn: false)) {
-  on<ToggleLightEvent>((event,emit){
-   emit(LightState(isOn: !state.isOn));
-  });
- }
+class LightBloc extends Bloc<LightEvent, LightState> {
+  LightBloc() : super(LightState(isOn: false)) {
+    on<ToggleLightEvent>((event, emit) {
+      emit(LightState(isOn: !state.isOn));
+    });
+  }
 }
