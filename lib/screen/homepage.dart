4d9dad0f8c3176039d5574f5ed519a842a30bc9b
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-05-14 16:33:38 +0500

    update homepage

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -26,11 +26,25 @@ class Homepage extends StatelessWidget {
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                  state.isOn ? Icons.lightbulb : Icons.lightbulb_outline,
-                  size: 100,
-                  color: state.isOn ? Colors.yellow : Colors.grey,
-                ),
+                Container(
+  decoration: BoxDecoration(
+    shape: BoxShape.circle,
+    boxShadow: [
+      BoxShadow(
+        color: state.isOn ? Colors.yellow.withValues(alpha:  0.2) : Colors.transparent,
+        blurRadius: 20,
+        spreadRadius: 5,
+        offset: Offset(0, 5),
+      ),
+    ],
+  ),
+  child: Icon(
+    state.isOn ? Icons.lightbulb : Icons.lightbulb_outline,
+    size: 150,
+    color: state.isOn ? Colors.yellow : Colors.grey,
+  ),
+),
+
                 SizedBox(height: 20),
                 Text(
                   state.isOn ? 'Light is ON' : 'Light is OFF',
